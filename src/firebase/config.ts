29c44090d39cc92958561928310d67576455d10d
Nayan Mitra <nayanmitra844@gmail.com>
2026-01-03 09:45:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2858685365-7c4e1",
+  "appId": "1:638711330660:web:8f70f41e5926f677d52c71",
+  "apiKey": "AIzaSyAQFYSit5vhnfZZgHy4Jh-ADeMUhHKxVhk",
+  "authDomain": "studio-2858685365-7c4e1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "638711330660"
+};
