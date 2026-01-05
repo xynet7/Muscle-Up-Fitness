2d889cee721bad8031732da8691369f6860ed83f
Nayan Mitra <nayanmitra844@gmail.com>
2026-01-05 20:01:20 +0000

    Try fixing this error: `Recoverable Error: Hydration failed because the

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,19 +3,21 @@ import Link from 'next/link';
 import { cn } from '@/lib/utils';
 
 export function Logo({ className, disableLink = false }: { className?: string, disableLink?: boolean }) {
-  const logoContent = (
-    <div className={cn('flex items-center gap-2 mr-6', className)}>
+  const image = (
       <Image src="https://iili.io/fjejDI1.png" alt="MuscleUp Logo" width={40} height={10} />
-    </div>
   );
 
   if (disableLink) {
-    return logoContent;
+    return (
+        <div className={cn('flex items-center gap-2 mr-6', className)}>
+            {image}
+        </div>
+    );
   }
 
   return (
-    <Link href="/">
-      {logoContent}
+    <Link href="/" className={cn('flex items-center gap-2 mr-6', className)}>
+      {image}
     </Link>
   );
 }
