a423224b83eb8c600570c617b52c990459417550
Nayan Mitra <nayanmitra844@gmail.com>
2026-01-05 19:36:23 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -79,11 +79,9 @@ export function Footer() {
         <div className="container flex flex-col items-center justify-between gap-4 py-6 md:h-20 md:flex-row md:py-0">
             <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
             <Logo />
-            {currentYear && (
-                <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-                © {currentYear} MuscleUp. All rights reserved.
-                </p>
-            )}
+            <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
+              {currentYear ? `© ${currentYear} MuscleUp. All rights reserved.` : `© ${new Date().getFullYear()} MuscleUp. All rights reserved.`}
+            </p>
             </div>
         </div>
       </div>
